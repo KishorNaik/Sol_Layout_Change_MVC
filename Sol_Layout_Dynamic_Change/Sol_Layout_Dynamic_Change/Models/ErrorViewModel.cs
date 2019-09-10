d41009efd5b98b62527fb99bd6e84f01a6f31565
Kishor Naik <kishor.naik011.net@gmail.com>
2019-09-10 18:07:00 +0530

    Code Commit

---
+++ Sol_Layout_Dynamic_Change/Sol_Layout_Dynamic_Change/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Sol_Layout_Dynamic_Change.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
